89e8a4997010f02c468e4ad6915bee80595098be
John <jhonstivensantiago@gmail.com>
2025-09-17 20:57:33 -0500

    Semana 04

---
+++ src/Semana04/Calculator.java
@@ -0,0 +1,12 @@
+package Semana04;
+
+public class Calculator {
+    public int Sumar(int numA , int numB) {
+        //paso por valor
+        return numA + numB ;
+    }
+        public int Multiplicar(int numA , int numB) {
+        //paso por valor
+        return numA * numB ;
+    }
+}
---
+++ src/Semana04/Main.java
@@ -0,0 +1,9 @@
+package Semana04;
+
+public class Main {
+    public static void main(String[] args) {
+    Calculator cal = new Calculator();
+    System.out.println("El resultado de la suma es: " + cal.Sumar(2,4));
+    System.out.println("El resultado0 de la multiplicacion es: " + cal.Multiplicar(5,7));   
+    }
+}
